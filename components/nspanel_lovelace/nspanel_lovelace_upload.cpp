6570e4b1f667f779c406caca2bad9b0656e86962
jacekowski <jacekowski+github@jacekowski.org>
2025-11-06 17:21:21 +0000

    Update nspanel_lovelace_upload.cpp

--- components/nspanel_lovelace/nspanel_lovelace_upload.cpp
+++ components/nspanel_lovelace/nspanel_lovelace_upload.cpp
@@ -191,7 +191,7 @@ int NSPanelLovelace::upload_by_chunks_(const std::string &url, int range_start) 
         ESP_LOGVV(TAG, "Write to UART successful");
         this->recv_ret_string_(recv_string, 5000, true);
         this->content_length_ -= read_len;
-        ESP_LOGD(TAG, "Uploaded %0.2f %%, remaining %d bytes, free heap $u bytes",
+        ESP_LOGD(TAG, "Uploaded %0.2f %%, remaining %d bytes, free heap %u bytes",
                  100.0 * (this->tft_size_ - this->content_length_) / this->tft_size_, this->content_length_, esp_get_free_heap_size());
         if (recv_string[0] != 0x05) {  // 0x05 == "ok"
           ESP_LOGD(
